8c0c19779976a322d303309c58febdf04c285753
vib1336 <vib1336@gmail.com>
2020-02-20 13:16:59 +0200

    Put view models in folders.

---
+++ 01. Shared Trip_Resources/SharedTrip/ViewModels/Trips/CreateTripModel.cs
@@ -0,0 +1,17 @@
+﻿namespace SharedTrip.ViewModels.Trips
+{
+    public class CreateTripModel
+    {
+        public string StartPoint { get; set; }
+
+        public string EndPoint { get; set; }
+
+        public string DepartureTime { get; set; }
+
+        public string ImagePath { get; set; }
+
+        public string Seats { get; set; }
+
+        public string Description { get; set; }
+    }
+}
---
+++ 01. Shared Trip_Resources/SharedTrip/ViewModels/Trips/DisplayAllTripViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SharedTrip.ViewModels.Trips
+{
+    using System.Collections.Generic;
+
+    public class DisplayAllTripViewModel
+    {
+        public IEnumerable<AllTripViewModel> Trips { get; set; }
+    }
+}
